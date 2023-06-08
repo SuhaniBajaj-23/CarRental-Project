dc2fcc49b4adf6a5d0c8061ae709d8762eb19910
SuhaniBajaj-23 <suhani.bajaj2020@vitstudent.ac.in>
2023-06-08 19:50:04 +0530

    some

--- admin/js/signupdb.js
+++ admin/js/signupdb.js
@@ -1,88 +1,5 @@
-// if (!window.indexedDB) {
-//     console.log(`Your browser doesn't support IndexedDB`);
-//     // return;
-// }
-
-// const request = indexedDB.open('carRental', 5);
-// let db;
-
-// request.onerror = (event) => {
-//     console.error(`Database error: ${event.target.errorCode}`);
-// };
-
-// request.onupgradeneeded = (event) => {
-//     db = event.target.result;
-
-//     if (!db.objectStoreNames.contains('Admins')) {
-//         db.createObjectStore('Admins', { keyPath: 'email', autoIncrement: true });
-//     }
-
-//     if (!db.objectStoreNames.contains('Users')) {
-//         db.createObjectStore('Users', { keyPath: 'email', autoIncrement: true });
-//     }
-// };
-
-// request.onsuccess = (event) => {
-//     db = event.target.result;
-//     console.log('Database opened successfully');
-
-//     // You can perform additional operations here or call relevant functions
-// };
-
-// function signup(userData, userType) {
-//     const transaction = db.transaction(userType, 'readwrite');
-//     const objectStore = transaction.objectStore(userType);
-
-//     let request = objectStore.add(userData);
-
-//     request.onsuccess = function (event) {
-//         console.log(`Successfully signed up as ${userType}`);
-//     };
-
-//     request.onerror = function (event) {
-//         console.error(`Error signing up as ${userType}: ${event.target.errorCode}`);
-//     };
-
-//     // transaction.oncomplete = function () {
-//     //     db.close();
-//     // };
-// }
-
-//  function click(){
-        
-//     var  name=document.querySelector('#name').value;
-//     var  password=document.querySelector('#password').value;
-//     var  email=document.querySelector('#email').value;
-//     var  cPassword=document.querySelector('#cpassword').value;
-//     var  selectedValue =  document.querySelector('#access');
-//     // var  value = selectedValue.options[selectedValue.selectedIndex].value;
-    
-//     // console.log(value);
-//     var Data = {
-//         name: name,
-//         email: email,
-//         password:password,
-//         cPassword:cPassword
-//     };
-
-//     console.log(Data);
-//     // signup(Data,'Admin')
-//     if(selectedValue="Admin"){
-//         signup(Data, 'Admins');
-//     }else{
-//         signup(Data,'Users');
-//     }
-
-//  }
-//  document.getElementById('submit').addEventListener('click', click);
-
-//  document.addEventListener('DOMContentLoaded', function () {
-//     document.getElementById('submit').addEventListener('click', click);
-// });
-
 if (!window.indexedDB) {
     console.log(`Your browser doesn't support IndexedDB`);
-    // return;
 }
 
 const request = indexedDB.open('carRental', 6);
@@ -107,9 +24,7 @@ request.onupgradeneeded = (event) => {
 request.onsuccess = (event) => {
     db = event.target.result;
     console.log('Database opened successfully');
-
-    // You can perform additional operations here or call relevant functions
-};
+}
 
 function signup(userData, userType) {
     const transaction = db.transaction(userType, 'readwrite');
@@ -125,9 +40,6 @@ function signup(userData, userType) {
         console.error(`Error signing up as ${userType}: ${event.target.errorCode}`);
     };
 
-    // transaction.oncomplete = function () {
-    //     db.close();
-    // };
 }
 
  function clickA(){
@@ -136,6 +48,13 @@ function signup(userData, userType) {
     var  password=document.querySelector('#password').value;
     var  email=document.querySelector('#email').value;
     var  cPassword=document.querySelector('#cpassword').value;
+    var passwordStrength = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})');
+
+    if(!passwordStrength.test(password)){
+        alert('Password should contain 1 uppercase letter, 1 lowercase letter, 1 numerical, 1 special character and should be atleast 8 characters long');
+    }else if (password !== cPassword) {
+        alert('please enter the same password in confirm password');
+    }
     
     var Data = {
         name: name,
@@ -155,7 +74,14 @@ function signup(userData, userType) {
     var  password=document.querySelector('#password').value;
     var  email=document.querySelector('#email').value;
     var  cPassword=document.querySelector('#cpassword').value;
+    var passwordStrength = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})');
     
+    if(!passwordStrength.test(password)){
+        alert('Password should contain 1 uppercase letter, 1 lowercase letter, 1 numerical, 1 special character and should be atleast 8 characters long');
+    }else if (password !== cPassword) {
+        alert('please enter the same password in confirm password');
+    }
+
     var Data = {
         name: name,
         email: email,
@@ -166,10 +92,4 @@ function signup(userData, userType) {
     signup(Data,'Users');
     window.location.href = '../loginU.html';
     
-
  }
-//  document.getElementById('submit').addEventListener('click', click);
-
-//  document.addEventListener('DOMContentLoaded', function () {
-//     document.getElementById('submit').addEventListener('click', click);
-// });
