e2d1508968f47dda4e252f9b786363681e28ab2a
SuhaniBajaj-23 <suhani.bajaj2020@vitstudent.ac.in>
2023-06-09 01:22:43 +0530

    all logins

--- admin/js/addCar.js
+++ admin/js/addCar.js
@@ -1,77 +1,103 @@
-const request = indexedDB.open('carDatabase', 2);
-let db;
+var carForm = document.querySelector('.fields');
+var carsData = function(){
+    var idb = indexedDB.open('carDatabase', 3);
 
-request.onerror = function(event) {
-  console.log('Error opening database');
+    idb.onerror = function(e){
+        console.log('error opening db!');
+    }
+
+    request.onsuccess = function(event) {
+    db = event.target.result;
+    console.log('Database opened successfully');
+    };
+
+    idb.onupgradeneeded=function (){
+        var request = idb.result;
+        console.log('Database upgraded successfully');
+        request.createObjectStore('cars', { autoIncrement: true })
+    };
+
+    idb.onsuccess=function(e){
+        var request =idb.result;
+        var tx=request.transaction('cars','readwrite');
+        var store = tx.objectStore('cars');
+        if(checkEmpty()){
+            alert('car added successfully');
+            store.put({
+                name: carForm[0].value,
+                price: carForm[1].value,
+                number: carForm[2].value,
+            });
+            displayCars();
+            location.reload();
+        }else{
+            alert('fill all deatils');
+            e.preventDefault();
+        }
+    };
 };
 
-request.onsuccess = function(event) {
-  db = event.target.result;
-  console.log('Database opened successfully');
-  displayCars();
-};
 
-request.onupgradeneeded = function(event) {
-  db = event.target.result;
-  console.log('Database upgraded successfully');
+function checkEmpty() {
+    return (
+      carForm[0].value != '' &&
+      carForm[1].value != '' &&
+      carForm[2].value != '' 
+    );
+  }
 
-  // Create an object store for cars
-  const objectStore = db.createObjectStore('cars', { keyPath: 'id', autoIncrement: true });
-};
+// var request = indexedDB.open('carDatabase', 2);
+// var db;
 
-// Handle form submission
-const carForm = document.getElementById('carForm');
-carForm.addEventListener('submit', function(event) {
-  event.preventDefault();
+// request.onerror = function(event) {
+//   console.log('Error opening database');
+// };
 
-  // Get input values
-  const name = document.querySelector('name').value;
-  const price = document.querySelector('price').value;
-  const number = document.querySelector('number').value;
-//   const color = document.getElementById('color').value;
+// request.onsuccess = function(event) {
+//   db = event.target.result;
+//   console.log('Database opened successfully');
+//   displayCars();
+// };
 
-  // Save car to IndexedDB
-  const transaction = db.transaction(['cars'], 'readwrite');
-  const objectStore = transaction.objectStore('cars');
-  const car = { name: name, price: price, number: number};
-  const request = objectStore.add(car);
+// request.onupgradeneeded = function(event) {
+//   db = event.target.result;
+//   console.log('Database upgraded successfully');
 
-  request.onsuccess = function(event) {
-    console.log('Car added to IndexedDB');
-    displayCars(); // Update the displayed cars
-  };
+//   // Create an object store for cars
+//   const objectStore = db.createObjectStore('cars', { keyPath: 'id', autoIncrement: true });
+// };
 
-  // Reset the form
-  carForm.reset();
-});
+// // Handle form submission
+
+// carForm.addEventListener('submit', function(event) {
+//   event.preventDefault();
+
+//   // Get input values
+//   const name = document.querySelector('#name').value;
+//   const price = document.querySelector('#price').value;
+//   const number = document.querySelector('#number').value;
+// //   const color = document.getElementById('color').value;
+
+//   // Save car to IndexedDB
+//   const transaction = db.transaction(['cars'], 'readwrite');
+//   const objectStore = transaction.objectStore('cars');
+//   const car = { name: name, price: price, number: number};
+//   const request = objectStore.add(car);
+
+//   request.onsuccess = function(event) {
+//     console.log('Car added to IndexedDB');
+//     displayCars(); // Update the displayed cars
+//   };
+
+//   // Reset the form
+//   carForm.reset();
+// });
 
 // Display all cars
 function displayCars() {
 
-//   const carTableBody = document.getElementById('cards');
-//   carTableBody.innerHTML = '';
-
-//   const objectStore = db.transaction('cars').objectStore('cars');
-//   objectStore.openCursor().onsuccess = function(event) {
-//     const cursor = event.target.result;
-//     if (cursor) {
-//       const car = cursor.value;
-
-//       // Create a new row in the table with the car details
-//       const newRow = document.createElement('card');
-//       newRow.innerHTML = `
-//         <td>${car.name}</td>
-//         <td>${car.price}</td>
-//         <td>${car.number}</td>
-//       `;
-//       carTableBody.appendChild(newRow);
-
-//       cursor.continue();
-//     }
-//   };
-
     const grid = document.querySelector('cards');
-    const transaction = db.transaction(['cars'],'readonly');
+    const transaction = db.transaction('cars','readonly');
     const objectStore = transaction.objectStore('cars');
     const request = objectStore.getAll();
 
@@ -82,7 +108,7 @@ function displayCars() {
             grid.innerHTML +=`
             <div class="card">
                             <div class="image">
-                                <img style="height: 200px; width: 400px;" src="assets\car9\car2.png">
+                                <img style="height: 200px; width: 400px;" src="../assets\car9\car2.png">
                             </div><br>
                             <div class="label">
                                 <div class="top">
@@ -103,3 +129,17 @@ function displayCars() {
         }
     }
 }
+
+
+function removeCar(id) {
+    const transactions = db.transaction('cars', 'readwrite');
+    const objectStores = transactions.objectStore('cars');
+    const request = objectStores.delete(id);
+  
+    request.onsuccess = function (event) {
+      console.log('Car removed from IndexedDB');
+      // Remove the card from the grid
+      const card = event.target.result;
+      card.parentNode.removeChild(card);
+    };
+  }
