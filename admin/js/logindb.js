9edf35c72945bebe56a6f671b37ff4f5f9bfe634
SuhaniBajaj-23 <suhani.bajaj2020@vitstudent.ac.in>
2023-06-09 16:26:10 +0530

    all logins

--- admin/js/logindb.js
+++ admin/js/logindb.js
@@ -1,24 +1,27 @@
-    function getAdmin(db) {
-        var email = document.querySelector("#email").value;
-        var password = document.querySelector("#password").value;
-    
-        console.log("About to login ");
-    
-        var transaction = db.transaction(["Admins"]); //readonly
-        var objectStore = transaction.objectStore("Admins");
-        var request = objectStore.get(email);
-    
-        request.onerror = function(e) {
-        alert("Admin does not exist");
-        //  return;
-        };
-        request.onsuccess = function(e) {
-        alert(password + " " + request.result.password);
-        if(password != request.result.password) {
-        alert("Invalid Credentials");
-        return;
-        } 
-        console.log("Successfully logged in");
-        window.location.href='../addCar.html'
-        };
-    }
+   
+    var loginForm = document.querySelector('#loginForm');
+    var loginAction = function () {
+    var idb = indexedDB.open('carRental', 2);
+
+    idb.onsuccess = function (e) {
+    var request = idb.result;
+    var tx = request.transaction('Admins', 'readonly');
+    var store = tx.objectStore('Admins');
+    var cursor = store.openCursor();
+    cursor.onsuccess = function () {
+      let currRes = cursor.result;
+      if (
+        (currRes.value.email == loginForm[0].value) &&
+        currRes.value.password == loginForm[1].value
+      ) {
+        console.log(currRes.value);
+        localStorage.setItem('code', 'secret');
+        localStorage.setItem('userKey', JSON.stringify(currRes.key));
+        window.location.href = '../mybooking.html';
+      } else {
+        currRes.continue();
+        console.log('Enter valid details');
+      }
+    };
+  };
+};
---
+++ user/js/mybooking.js
@@ -0,0 +1,5 @@
+
+function logout(){
+    var logout = localStorage.setItem("code", "logout")
+    window.location.href = "../../index.html"
+}
