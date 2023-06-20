f35b1d2913a4fa1f73ed48a32e5c57fb8b52d5be
SuhaniBajaj-23 <suhani.bajaj2020@vitstudent.ac.in>
2023-06-20 20:14:57 +0530

    analytics left

--- js/orders.js
+++ js/orders.js
@@ -14,14 +14,14 @@ setTimeout(()=>{
               let divv = document.createElement('div');
               divv.innerHTML = `<div class="cards">
                                     <div class="image">
-                                        <img src=${car.image} style="height: 250px; width: 400px;">
+                                        <img src=${car.image} style="height: 200px; width: 350px;">
                                     </div>
                                     <div class="deets">
                                         <div class="carname">${car.name}</div>
                                         <div class="price">Total Fare - $${car.totalFare}</div>
                                         <div class="flex">
                                         <div class="frompick">From : ${car.address}</div>
-                                        <div class="dates">${car.pickDate} - ${car.dropDate}</div>
+                                        <div class="dates">${car.pickDate} to ${car.dropDate}</div>
                                         </div>
                                         <div class="seater">
                                         <div class="number">Booked On: ${car.time}</div>
